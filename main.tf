d357e4d1720e01168f3937d07c10e861a3da4953
ardhmd <ard.datascientest@gmail.com>
2023-10-05 16:32:40 +0000

    Update version

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ module "aws_vpc" {
 
 # EKS Cluster Configuration
 module "eks_cluster" {
-  source              = "github.com/ard-hmd/terraform-aws-eks-cluster.git"
-  cluster_config      = {
+  source = "github.com/ard-hmd/terraform-aws-eks-cluster.git"
+  cluster_config = {
     name    = "eks-cluster-spring-petclinic"
-    version = "1.27"
+    version = "1.28"
   }
   public_subnets_ids  = module.aws_vpc.public_subnets_ids
   private_subnets_ids = module.aws_vpc.private_subnets_ids
@@ -23,8 +23,8 @@ module "eks_cluster_nodegroup" {
   private_subnets_ids = module.aws_vpc.private_subnets_ids
   eks_cluster_name    = module.eks_cluster.eks_cluster_name
 
-   # Node Group Details
-  node_groups         = [
+  # Node Group Details
+  node_groups = [
     {
       name           = "nodegroup-ondemand-1"
       ami_type       = "AL2_x86_64"
@@ -33,7 +33,7 @@ module "eks_cluster_nodegroup" {
       disk_size      = 20
       scaling_config = {
         desired_size = 2
-        max_size     = 3
+        max_size     = 2
         min_size     = 1
       }
     }
@@ -46,67 +46,67 @@ module "eks_cluster_nodegroup" {
 module "rds_instance" {
   source     = "github.com/ard-hmd/terraform-aws-rds-with-replica.git"
   aws_region = var.aws_region
-  
+
   database_configurations = [
     {
-     identifier              = "customersdb"
-     allocated_storage       = 10
-     engine_version          = "8.0"
-     instance_class          = "db.t3.micro"
-     db_name                 = "customersdb"
-     db_username             = "admin"
-     db_password             = var.customersdb_password
-     parameter_group_name    = "default.mysql8.0"
-     db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
-     skip_final_snapshot     = true
-     publicly_accessible     = false
-     backup_retention_period = 0
-     vpc_id                  = module.aws_vpc.vpc_id
-     allowed_cidrs           = [module.aws_vpc.vpc_cidr]
-     multi_az                = true
-     sg_name                 = "customers-db-sg"
-     sg_description          = "Security Group for customersdb"
-   },
-   # Vets Database Configuration
-   {
-     identifier              = "vetsdb"
-     allocated_storage       = 10
-     engine_version          = "8.0"
-     instance_class          = "db.t3.micro"
-     db_name                 = "vetsdb"
-     db_username             = "admin"
-     db_password             = var.vetsdb_password
-     parameter_group_name    = "default.mysql8.0"
-     db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
-     skip_final_snapshot     = true
-     publicly_accessible     = false
-     backup_retention_period = 0
-     vpc_id                  = module.aws_vpc.vpc_id
-     allowed_cidrs           = [module.aws_vpc.vpc_cidr]
-     multi_az                = true
-     sg_name                 = "vets-db-sg"
-     sg_description          = "Security Group for vetsdb"
-   },
-   # Visits Database Configuration
-   {
-     identifier              = "visitsdb"
-     allocated_storage       = 10
-     engine_version          = "8.0"
-     instance_class          = "db.t3.micro"
-     db_name                 = "visitsdb"
-     db_username             = "admin"
-     db_password             = var.visitsdb_password
-     parameter_group_name    = "default.mysql8.0"
-     db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
-     skip_final_snapshot     = true
-     publicly_accessible     = false
-     backup_retention_period = 0
-     vpc_id                  = module.aws_vpc.vpc_id
-     allowed_cidrs           = [module.aws_vpc.vpc_cidr]
-     multi_az                = true
-     sg_name                 = "visits-db-sg"
-     sg_description          = "Security Group for visitsdb"
-   },
+      identifier              = "customersdb"
+      allocated_storage       = 10
+      engine_version          = "8.0"
+      instance_class          = "db.t3.micro"
+      db_name                 = "customersdb"
+      db_username             = "admin"
+      db_password             = var.customersdb_password
+      parameter_group_name    = "default.mysql8.0"
+      db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
+      skip_final_snapshot     = true
+      publicly_accessible     = false
+      backup_retention_period = 0
+      vpc_id                  = module.aws_vpc.vpc_id
+      allowed_cidrs           = [module.aws_vpc.vpc_cidr]
+      multi_az                = true
+      sg_name                 = "customers-db-sg"
+      sg_description          = "Security Group for customersdb"
+    },
+    # Vets Database Configuration
+    {
+      identifier              = "vetsdb"
+      allocated_storage       = 10
+      engine_version          = "8.0"
+      instance_class          = "db.t3.micro"
+      db_name                 = "vetsdb"
+      db_username             = "admin"
+      db_password             = var.vetsdb_password
+      parameter_group_name    = "default.mysql8.0"
+      db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
+      skip_final_snapshot     = true
+      publicly_accessible     = false
+      backup_retention_period = 0
+      vpc_id                  = module.aws_vpc.vpc_id
+      allowed_cidrs           = [module.aws_vpc.vpc_cidr]
+      multi_az                = true
+      sg_name                 = "vets-db-sg"
+      sg_description          = "Security Group for vetsdb"
+    },
+    # Visits Database Configuration
+    {
+      identifier              = "visitsdb"
+      allocated_storage       = 10
+      engine_version          = "8.0"
+      instance_class          = "db.t3.micro"
+      db_name                 = "visitsdb"
+      db_username             = "admin"
+      db_password             = var.visitsdb_password
+      parameter_group_name    = "default.mysql8.0"
+      db_subnet_group_name    = module.aws_vpc.rds_subnet_group_name
+      skip_final_snapshot     = true
+      publicly_accessible     = false
+      backup_retention_period = 0
+      vpc_id                  = module.aws_vpc.vpc_id
+      allowed_cidrs           = [module.aws_vpc.vpc_cidr]
+      multi_az                = true
+      sg_name                 = "visits-db-sg"
+      sg_description          = "Security Group for visitsdb"
+    },
   ]
   create_replica = false
 }
